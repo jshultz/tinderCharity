7843cb95e33ca467d8adc6062ffcad3d1cdb9f55
Jason Shultz <jasshultz@gmail.com>
2015-08-31 07:07:00 -0600

    Initial Commit

---
+++ tinderCharity/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tinderCharity
+//
+//  Created by Jason Shultz on 8/31/15.
+//  Copyright (c) 2015 Jason Shultz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tinderCharity/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tinderCharity
+//
+//  Created by Jason Shultz on 8/31/15.
+//  Copyright (c) 2015 Jason Shultz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
